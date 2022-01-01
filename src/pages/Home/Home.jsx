85dac2f89aad52c4158893593c731eb0efb91873
Opeyemi Oyeleke <topfemo@gmail.com>
2022-01-02 00:01:22 +0100

    add home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as BannerImage } from "../../assets/svg/banner-image.svg";
+import Navbar from "../../component/Navbar/Navbar";
+import "./home.scss";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+export default function Home() {
+  return (
+    <div className="home-container">
+      <Navbar />
 
-export default Home;
+      <section className="banner">
+        <div className="wrapper-banner">
+          <div className="banner-left">
+            <div className="row-one">Create & Manage Apartments Easily</div>
+            <div className="row-two">
+              Makes it easy for you to create apartments along with detailed
+              information of your choice.
+            </div>
+            <div className="row-three">
+              <Link to="/signup">
+                <div className="app-btn wide">Get started for free</div>
+              </Link>
+            </div>
+            <div className="row-four">
+              made with ❤️ by{" "}
+              <a href="https://github.com/opeoyeleke">@opeoyeleke</a>
+            </div>
+          </div>
+
+          <div className="banner-right">
+            <BannerImage className="banner-image" />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
